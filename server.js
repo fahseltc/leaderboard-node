afb816928e1e735f5481d5e5efe78ff4fd012b22
Charles Fahselt <Charles.Fahselt@gmail.com>
2018-02-19 22:20:52 -0600

    lets try express

--- server.js
+++ server.js
@@ -1,36 +1,44 @@
-var http = require('http');
-var Router = require('node-router');
+//var http = require('http');
+//var Router = require('node-router');
 const Database = require('./database.js');
-var bodyParser = require('body-parser');
+//var bodyParser = require('body-parser');
 
-var router = Router();
-var route = router.push;
+//var router = Router();
+//var route = router.push;
 var DB = new Database();
 
-var server = http.createServer(router).listen(process.env.PORT || 3000);
+//var server = http.createServer(router).listen(process.env.PORT || 3000);
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
 
 
-route('GET',  '/leaderboard/top_5', get_top_5);
-route('GET',  '/leaderboard/top_10', get_top_10);
-route('GET',  '/leaderboard', get_leaderboard);
-route('POST', '/leaderboard', bodyParser.urlencoded({extended: false}));
 
-route(function (req, res, next) {
-    console.log(req.body);
-    var body = JSON.parse(JSON.stringify(req.body));
+// route('GET',  '/leaderboard/top_5', get_top_5);
+// route('GET',  '/leaderboard/top_10', get_top_10);
+// route('GET',  '/leaderboard', get_leaderboard);
+// route('POST', '/leaderboard', bodyParser.urlencoded({extended: false}));
 
-    var score = body.score;
-    var name = body.name;
-    console.log(score);
-    console.log(name);
+// route(function (req, res, next) {
+//     console.log(req.body);
+//     var body = JSON.parse(JSON.stringify(req.body));
 
-    if(score && name) {
-        DB.write(score, name);
-        res.send(200);
-    } else {
-        res.send(422);
-    }
-});
+//     var score = body.score;
+//     var name = body.name;
+//     console.log(score);
+//     console.log(name);
+
+//     if(score && name) {
+//         DB.write(score, name);
+//         res.send(200);
+//     } else {
+//         res.send(422);
+//     }
+// });
 
 function get_leaderboard(req, res, next) {
     var data = DB.read();
