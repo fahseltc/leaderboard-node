d6dbb61eeeef3795d63e0256d093bc96f6085490
Charles Fahselt <Charles.Fahselt@gmail.com>
2018-02-20 18:09:07 -0600

    adding CORS

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.use(express.urlencoded({extended: true})); // to support URL-encoded bodies
 
 app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/leaderboard', get_leaderboard);
 app.get('/leaderboard/top_5', get_top_5);
